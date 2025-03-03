b53872543c79816ae07eb95441b179791e91917d
Ching-fung WAN <cfwan6@gmail.com>
2025-03-03 13:23:43 +0000

    post url

--- index.js
+++ index.js
@@ -22,3 +22,25 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+//post url
+app.post("/api/shorturl", function(req, res){
+  const url = req.body.url;
+
+if (!vaildUrl.isWebUri(url)){
+  return res.json({ error: 'invalid url' })
+} 
+
+let shortUrl = Object.keys(urlDatabase).find(key => urlDatabase[key] === url);
+
+if (!shortUrl){
+  shortUrl = counter++;
+  urlDatabase[shortUrl] = url;
+}
+
+res.json({
+  original_url: url,
+  short_url: shortUrl
+})
+
+});
